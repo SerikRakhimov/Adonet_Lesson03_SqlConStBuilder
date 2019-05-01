ea2f348a1f5a92c3230965e6ef3d4e1dea47cba2
SerikRakhimov <s_astana@mail.ru>
2019-05-02 01:06:50 +0600

    first commit

---
+++ SqlConStBuilder/FormMain.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace SqlConStBuilder
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(331, 78);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(155, 22);
+            this.textBoxLogin.TabIndex = 0;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(331, 142);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(155, 22);
+            this.textBoxPassword.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(229, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Логин:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(229, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Пароль:";
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(187, 217);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(337, 30);
+            this.buttonOk.TabIndex = 4;
+            this.buttonOk.Text = "Проверить подключение к базе данных";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 340);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxLogin);
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonOk;
+    }
+}
+
---
+++ SqlConStBuilder/FormMain.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlConStBuilder
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            SqlConnectionStringBuilder SqlConnBuild = new SqlConnectionStringBuilder();
+ 
+            if (textBoxLogin.Text != "" && textBoxPassword.Text != "")
+            {
+                SqlConnBuild.DataSource = @"(LocalDb)\MSSQLLocalDB";
+                SqlConnBuild.InitialCatalog = "InternetShop.DataContext";
+                SqlConnBuild.IntegratedSecurity = true;
+                SqlConnBuild.UserID = textBoxLogin.Text;
+                SqlConnBuild.Password = textBoxPassword.Text;
+                SqlConnection con = new SqlConnection(SqlConnBuild.ConnectionString);
+                try
+                {
+                    con.Open();
+                    if (con.State == ConnectionState.Open)
+                        MessageBox.Show("Успешное подключение!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Сообщение: " + "\n" + ex.Message);
+                }
+                finally
+                {
+                    con?.Close();
+                }
+
+            }
+        }
+    }
+}
